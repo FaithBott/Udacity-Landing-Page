4a7b7c241af2c7765e1e827522522f9a81dd9771
Rebecca Bott <faithbott@gmail.com>
2019-12-22 19:52:33 -0800

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,71 @@
+/**
+ * 
+ * Manipulating the DOM exercise.
+ * Exercise programmatically builds navigation,
+ * scrolls to anchors from navigation,
+ * and highlights section in viewport upon scrolling.
+ * 
+ * Dependencies: None
+ * 
+ * JS Version: ES2015/ES6
+ * 
+ * JS Standard: ESlint
+ * 
+*/
+
+/**
+ * Global Vars
+ */
+
+const sections = document.getElementsByTagName("section");
+
+// loops through all the secitons 
+function createNavLi() {
+    for (const section of sections) {
+        createSectionLink(section)
+    }
+}
+
+
+// addings an item to the nav for every section 
+function createSectionLink(section) {
+    const node = document.createElement('li')
+    const nodeLink = document.createElement('a')
+    nodeLink.onclick = function () {
+        section.scrollIntoView()
+    }
+    node.id = section.id + 'nav'
+    nodeLink.innerText = section.getAttribute('data-nav')
+    node.appendChild(nodeLink);
+    document.getElementById('navbar__list').appendChild(node);
+}
+
+function setActive(id) {
+    console.log(id)
+    const navlist = document.getElementsByTagName("li")
+    for (const navitem of navlist) {
+        navitem.classList.remove("active")
+    }
+    document.getElementById(id).classList.add("active");
+}
+
+
+// Runs the fuction to create the nav bar 
+createNavLi();
+
+
+// Set sections as active
+window.addEventListener("scroll", (e) => {
+    const pos = window.scrollY;
+
+    // start at the bottom so that the top section in view is active
+    for (let section of Array.from(sections).reverse()) {
+        const offsetBottom = section.offsetTop + section.clientHeight;
+        if (pos < section.offsetTop && pos < offsetBottom) {
+            console.log({ pos, topPixel: section.offsetTop, id: section.id })
+            setActive(section.id + 'nav')
+        }
+    }
+
+})
+
